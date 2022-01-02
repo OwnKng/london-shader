645a4e2f2de40eafa49c0f5af389e10c30878d13
OwnKng <owen.king@outlook.com>
2022-01-02 12:27:46 +0000

    Waves

--- src/shaders/fragmentShader.ts
+++ src/shaders/fragmentShader.ts
@@ -1,11 +1,20 @@
+import { hsl2rgb } from "./hsl2rgb"
+
 export const fragmentShader = /* glsl */ `
     varying float vStrength; 
     varying vec2 vUv; 
+    varying float vElevation; 
+    varying float vRings; 
+
+    ${hsl2rgb}
 
     void main() {
-        float alpha = 1.0 - step(0.5, distance(vUv, vec2(0.5)));
+        float color = vElevation * 0.2;
+        float ringsStep = mod(vRings * 10.0, 2.5);
+
+        vec3 finalColor = hsl2rgb(0.6, 0.5, 0.5 + 1.0 - ringsStep * 0.5); 
         if(vStrength < 0.6) discard; 
 
-        gl_FragColor = vec4(vStrength, vStrength, vStrength, 1.0);
+        gl_FragColor = vec4(finalColor, 1.0);
     }
 `
--- src/shaders/vertexShader.ts
+++ src/shaders/vertexShader.ts
@@ -11,6 +11,8 @@ export const vertexShader = /* glsl */ `
 
     varying float vStrength; 
     varying vec2 vUv; 
+    varying float vElevation; 
+    varying float vRings; 
 
     float random(float n) {
 	    return fract(sin(n) * 43758.5453123);
@@ -26,22 +28,27 @@ export const vertexShader = /* glsl */ `
         float strength = color.r * 0.30 + color.g * 0.30 + color.b * 0.40; 
 
         //_ distort the positions
-        float rings = 1.0 - mod(distance(particleUv, vec2(0.5)) * 10.0, 1.0);
-        float randomNoise = cnoise(vec3(particleUv, pindex));
-        displaced.z += (rings * randomNoise) * 5.0;
+        float randomNoise = cnoise(vec3(2.0, particleUv)) * 0.25;
+        float rings = distance(particleUv + randomNoise, vec2(0.0, 0.2));
+        rings = mod(rings * 2.0, 1.0);
+        float elevation = ceil(rings * 4.0); 
+ 
+        // displaced.z += elevation;
 
         //_ size 
-        float psize = cnoise(vec3(particleUv, pindex)) + 2.0;
-        psize *= 0.4; 
-        psize *= max(strength, 0.2);
+        // float psize = cnoise(vec3(particleUv, mod(rings * 10.0, 2.5))) + 2.0;
+        // psize *= 0.4; 
+        // psize *= max(strength, 0.1);
 
         //_ final position
         vec4 transformedPosition = modelViewMatrix * vec4(displaced, 1.0); 
-        transformedPosition.xyz += position * psize; 
+        transformedPosition.xyz += position * 0.6;
         gl_Position = projectionMatrix * transformedPosition;
  
         //_ passing the varyings
         vStrength = strength; 
         vUv = adjustedUv; 
+        vElevation = elevation; 
+        vRings = rings; 
     }
 `
